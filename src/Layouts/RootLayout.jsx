0cb74d30fad4c9ab2920b55f961b4763ef7775f9
jazeelius <42842694+jazeelius@users.noreply.github.com>
2024-07-03 16:03:22 -0400

    working on the navbar

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -5,48 +5,28 @@ function RootLayout() {
   return (
     <>
       {/* Remix */}
-      <header className="bg-white">
-        <nav className="">
-          <div>
-            <NavLink to="/">
-              <h2 className="font-[Poppins] text-black">Callai</h2>
-            </NavLink>
+      <div className="min-h-screen flex flex-col bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee] h-screen text-white text-sm sm:text-base">
+        <header>
+          <nav className="block w-full max-w-screen-xl px-4 py-3 mx-auto text-white shadow-md rounded-xl bg-white">
             <div>
-              <NavLink to="/test"></NavLink>
-            </div>
-          </div>
-        </nav>
-      </header>
-      <main>
-        <Outlet />
-      </main>
+              <NavLink to="/">
+                <h2 className="mr-4 ml-2 cursor-pointer py-1.5 font-sans text-3xl font-extrabold leading-relaxed tracking-normal text-black antialiased">
+                  Callai
+                </h2>
+              </NavLink>
 
-      {/* <div className='root-layout'>
-      <div className='root-layout'>
-  <header className='max-w-screen-xl mx-auto p-4'>
-    <nav className='bg-white border-b border-gray-200 dark:bg-gray-900 dark:border-gray-800 flex items-center justify-between'>
-      <h1 className='text-2xl font-bold text-gray-900 dark:text-white'>
-        Jobarouter
-      </h1>
-      <ul className='flex space-x-4'>
-        <li className='py-2'>
-          <NavLink to="/" className='text-white bg-blue-700 px-3 py-2 rounded md:bg-transparent md:text-blue-700 dark:text-white md:dark:text-blue-500'>
-            Home
-          </NavLink>
-        </li>
-        <li className='py-2'>
-          <NavLink to="/test" className='text-gray-900 dark:text-white hover:text-blue-700 dark:hover:text-blue-500 px-3 py-2 rounded'>
-            Try it out
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
-  <main>
-    <Outlet/>
-  </main>
-</div>
-    </div> */}
+              <div className="flex gap-1 ml-auto md:mr-4">
+                <button>
+                  <NavLink to="/test">Try it out now</NavLink>
+                </button>
+              </div>
+            </div>
+          </nav>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </>
   );
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function LandingPage() {
   return (
-    <main className='min-h-screen flex flex-col bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee] h-screen text-white text-sm sm:text-base'>
+    <main>
         hi
     </main>
   )
