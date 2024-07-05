1adbc155fc0ba04375734b2b327e1e90ec2b854e
jazeelius <42842694+jazeelius@users.noreply.github.com>
2024-07-05 08:19:29 -0400

    endorsement section before experiment

--- src/components/Endorsements.jsx
+++ src/components/Endorsements.jsx
@@ -4,18 +4,18 @@ function Endorsements() {
   return (
     <>
       <section className="">
-    <div class="py-8 lg:py-16 mx-auto max-w-screen-xl px-4 bg-[#FAFAFA]">
-        <h2 class="pt-20 mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-200 md:text-4xl">You’ll be in good company</h2>
-        <div class="flex text-gray-500 justify-center hover:bg gap-4">
+    <div className="py-8 lg:py-16 mx-auto max-w-screen-xl px-4 bg-[#FAFAFA]">
+        <h2 className="pt-20 mb-8 lg:mb-12 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-800 md:text-4xl">You’ll be in good company</h2>
+        <div className="mt-3 flex md:col-span-2 text-gray-500 justify-center hover:bg gap-4">
             
-            <a href="#" class="flex justify-center items-center m-5 text-5xl">
-            <i class="fa-brands fa-salesforce"></i>                                               
+            <a href="#" className="flex justify-center items-center m-5 text-5xl hover:text-black">
+            <i className="fa-brands fa-salesforce"></i>                                               
             </a>
-            <a href="#" class="flex justify-center items-center m-5 text-5xl">
-            <i class="fa-brands fa-google"></i>                                                                 
+            <a href="#" className="flex justify-center items-center m-5 text-5xl hover:text-black">
+            <i className="fa-brands fa-google"></i>                                                                 
             </a>
-            <a href="#" class="flex justify-center items-center m-5 text-5xl">
-            <i class="fa-brands fa-stripe"></i>                                                 
+            <a href="#" className="flex justify-center items-center m-5 text-5xl hover:text-black">
+            <i className="fa-brands fa-stripe"></i>                                                 
             </a>
 
            
