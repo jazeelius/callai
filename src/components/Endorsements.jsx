5ae078f5485a5c1987b1e430f814d3741863558e
jazeelius <42842694+jazeelius@users.noreply.github.com>
2024-07-05 08:28:59 -0400

    Added tooltips to endorsements

--- src/components/Endorsements.jsx
+++ src/components/Endorsements.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function Endorsements() {
   return (
     <>
@@ -7,16 +8,35 @@ function Endorsements() {
     <div className="py-8 lg:py-16 mx-auto max-w-screen-xl px-4 bg-[#FAFAFA]">
         <h2 className="pt-20 mb-8 lg:mb-12 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-800 md:text-4xl">You’ll be in good company</h2>
         <div className="mt-3 flex md:col-span-2 text-gray-500 justify-center hover:bg gap-4">
+        
+
+            <div className="group relative flex justify-center">
+            <a href="#" className="flex justify-center items-center m-5 text-5xl hover:text-black">
+            <i className="fa-brands fa-salesforce"></i>    
+            <span class="absolute top-20 scale-0 transition-all rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100">SalesForce</span>                                           
+            </a>
+            </div>
+
+            <div className="group relative flex justify-center">
+            <a href="#" className="flex justify-center items-center m-5 text-5xl hover:text-black">
+            <i className="fa-brands fa-google"></i>    
+            <span class="absolute top-20 scale-0 transition-all rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100">Google</span>                                           
+            </a>
+            </div>
+
+            <div className="group relative flex justify-center">
+            <a href="#" className="flex justify-center items-center m-5 text-5xl hover:text-black">
+            <i className="fa-brands fa-stripe"></i>    
+            <span class="absolute top-20 scale-0 transition-all rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100">Stripe</span>                                           
+            </a>
+            </div>
             
-            <a href="#" className="flex justify-center items-center m-5 text-5xl hover:text-black">
-            <i className="fa-brands fa-salesforce"></i>                                               
-            </a>
-            <a href="#" className="flex justify-center items-center m-5 text-5xl hover:text-black">
+            {/* <a href="#" className="flex justify-center items-center m-5 text-5xl hover:text-black">
             <i className="fa-brands fa-google"></i>                                                                 
-            </a>
-            <a href="#" className="flex justify-center items-center m-5 text-5xl hover:text-black">
+            </a> */}
+            {/* <a href="#" className="flex justify-center items-center m-5 text-5xl hover:text-black">
             <i className="fa-brands fa-stripe"></i>                                                 
-            </a>
+            </a> */}
 
            
            
