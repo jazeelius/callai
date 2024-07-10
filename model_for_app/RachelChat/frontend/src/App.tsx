f679775da644d75d8ab12e3a429338e86c085ba7
jazeelius <42842694+jazeelius@users.noreply.github.com>
2024-07-10 12:43:58 -0400

    added new folder model

---
+++ model_for_app/RachelChat/frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import Controller from "./components/Controller"
+
+function App() {
+ 
+
+  return (
+
+      <div className='App'>
+        <Controller/>
+      </div>
+
+  )
+}
+
+export default App
